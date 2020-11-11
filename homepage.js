e1fd347cc67eab926a086168dc88b15f88ac4472
chensun113 <59670255+chensun113@users.noreply.github.com>
2020-11-10 23:14:23 -0500

    Update homepage.js
    Function of get city name data from API and call it as function  displayStatus(data, city);

--- homepage.js
+++ homepage.js
@@ -7,3 +7,38 @@ var clearButtonEl = document.querySelector('#clearbutton')
 var key= "c460dd5c15d9df993585c0bf2401e2ca";//create account webpage
 var UVIndex;
 var historyCount=0;
+var getCityStatus = function (city) {
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+key;
+//get API 
+  fetch(apiUrl)//拿到API，then开始response function，回应「」
+  //OK is boolean 值
+    .then(function (response) {
+      if (response.ok) {//true，回应出里面的JSON（）
+        response.json().then(function (data) {//jSON的数据变为DATA 在function里
+          displayStatus(data, city);//API里拿到的最终数据，diaplayStatus is a
+          
+          var temp = document.getElementById(data.city.name);
+       //save history as button
+          if(temp == null){
+            var historyButton= document.createElement('button');
+            historyButton.setAttribute("cityName",data.city.name);
+            historyButton.setAttribute("id",data.city.name);
+            historyButton.setAttribute("class","btn");
+            historyButton.textContent=data.city.name;
+            save(data.city.name);
+            languageButtonsEl.appendChild(historyButton);
+          }
+          
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to OpenWeather');
+    });
+};
+function getUnIndex_1(data){
+  UVIndex=data;
+  return data;
+}
